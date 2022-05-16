a97fbb870865a374dc1e635ea63a28f9a1886361
pedrolmr <moverapedro@gmail.com>
2022-05-16 06:33:55 -0400

    added wave effect with js

--- FormWaveAnimation/script.js
+++ FormWaveAnimation/script.js
@@ -1,1 +1,8 @@
-console.log("scriptjs");
+const labels = document.querySelectorAll(".form-control label");
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split("")
+        .map((letter, index) => `<span style="transition-delay: ${index * 30}ms">${letter}</span>`)
+        .join("");
+})
